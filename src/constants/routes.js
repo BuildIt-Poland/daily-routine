bf5c262337a9d396fb70630c5f40250cb839c2a7
Maciej Hein <maciej.hein@wipro.com>
2019-06-10 13:36:57 +0200

    change urls to simpler

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,4 @@
 export const ROOT_PATH = '/';
-export const FRONT_END_PATH = '/front-end';
-export const BACK_END_PATH = '/back-end';
-export const DEV_OPS_PATH = '/dev-ops';
+export const FRONT_END_PATH = '/frontend';
+export const BACK_END_PATH = '/backend';
+export const DEV_OPS_PATH = '/devops';
