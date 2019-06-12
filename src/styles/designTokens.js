ab3c622da052f768313107daf94b59309e1a4ecc
blurbyte <mateusz.lachacz@wipro.com>
2019-06-12 12:18:25 +0200

    Downscale fonts and spacing a bit

--- src/styles/designTokens.js
+++ src/styles/designTokens.js
@@ -18,21 +18,21 @@ export const colorYellow = '#f5ca1d';
 export const colorDarkYellow = '#d18b13';
 
 // Spacing (mainly for paddings and margins)
-export const spacingLarge = '3rem';
-export const spacingMedium = '2rem';
-export const spacingSmall = '1rem';
+export const spacingLarge = '2.4rem';
+export const spacingMedium = '1.6rem';
+export const spacingSmall = '0.8rem';
 export const borderWidthThick = '2px';
 export const borderWidthThin = '1px';
 
 // Radius
 export const bordeRadiusCircle = '50%';
-export const borderRadius = '0.6rem';
+export const borderRadius = '0.4rem';
 
 // Font size
-export const fontSmall = '1.4rem';
-export const fontMedium = '1.8rem';
-export const fontLarge = '2.4rem';
-export const fontXLarge = '3rem';
+export const fontSmall = '1.0rem';
+export const fontMedium = '1.4rem';
+export const fontLarge = '1.8rem';
+export const fontXLarge = '2.2rem';
 
 // Font weight
 export const fontWeightNormal = 400;
