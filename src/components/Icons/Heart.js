dff4b474fdb261ff4831224c4029e0ae82b03351
blurbyte <mateusz.lachacz@wipro.com>
2019-06-12 12:18:35 +0200

    Add landing page assets

---
+++ src/components/Icons/Heart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Heart() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11">
+      <path
+        fill="#222"
+        opacity="0.2"
+        d="M6 11S0 7.336 0 3.075C0 1.377 1.343 0 3 0s3 1.377 3 3.075C6 1.377 7.343 0 9 0s3 1.377 3 3.075C12 7.336 6 11 6 11"
+      />
+    </svg>
+  );
+}
+
+export default Heart;
--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,7 +1,12 @@
+// Logo
+export { default as Logo } from './Logo';
+
 // General
 export { default as Gear } from './Gear';
 export { default as LongArrow } from './LongArrow';
 export { default as CopyCards } from './CopyCards';
+export { default as Heart } from './Heart';
+export { default as GithubLogo } from './GithubLogo';
 
 // Buttons icons
 export { default as Lightbulb } from './Lightbulb';
