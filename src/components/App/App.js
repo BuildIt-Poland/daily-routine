a4c8f75712868d1687066c6a484762ad31e9e6d3
Marcin Goławski <marcin.golawski@wipro.com>
2019-06-12 15:50:34 +0200

    [DR-73] Theming for Role Pages (#16)
    * Add initial theming for colours
    
    * Add tests for themeGenerator
    
    * Code polishing
    
    * Fix case sensitive routing
    
    * Change wrapper name

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
+import { Route, withRouter } from 'react-router-dom';
 
+import generateTheme from '../../styles/generateTheme';
 import { ROOT_PATH, FRONT_END_ROLE_PATH, BACK_END_ROLE_PATH, DEV_OPS_ROLE_PATH } from '../../constants/routes';
 import LandingPage from '../LandingPage';
 import FrontEndRolePage from '../FrontEndRolePage';
 import BackEndRolePage from '../BackEndRolePage';
 import DevOpsRolePage from '../DevOpsRolePage';
 
-function App() {
+const Main = styled.main`
+  background-color: ${props => props.theme.primaryColor};
+`;
+
+function App({ location }) {
   return (
-    <main>
-      <Route exact path={ROOT_PATH} component={LandingPage} />
-      <Route exact path={FRONT_END_ROLE_PATH} component={FrontEndRolePage} />
-      <Route exact path={BACK_END_ROLE_PATH} component={BackEndRolePage} />
-      <Route exact path={DEV_OPS_ROLE_PATH} component={DevOpsRolePage} />
-    </main>
+    <ThemeProvider theme={generateTheme(location.pathname)}>
+      <Main>
+        <Route exact path={ROOT_PATH} component={LandingPage} />
+        <Route exact path={FRONT_END_ROLE_PATH} component={FrontEndRolePage} />
+        <Route exact path={BACK_END_ROLE_PATH} component={BackEndRolePage} />
+        <Route exact path={DEV_OPS_ROLE_PATH} component={DevOpsRolePage} />
+      </Main>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default withRouter(App);
