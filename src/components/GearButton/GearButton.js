efde9180fcea13bff134f4a5552d4685bb6e432e
Marcin Goławski <marcin.golawski@wipro.com>
2019-06-14 12:31:06 +0200

    Fix import order

--- src/components/GearButton/GearButton.js
+++ src/components/GearButton/GearButton.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Gear } from '../Icons';
 import Button from '../Button';
+import { Gear } from '../Icons';
 
 function GearButton({ onClick = () => {} }) {
   return (
