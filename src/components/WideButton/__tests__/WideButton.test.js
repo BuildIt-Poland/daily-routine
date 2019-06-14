3aa0471d6f9d63d3fe1d6151fd75324fc4af5109
Maciej Hein <maciej.hein@wipro.com>
2019-06-14 12:45:10 +0200

    fix tests name

--- src/components/WideButton/__tests__/WideButton.test.js
+++ src/components/WideButton/__tests__/WideButton.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
 
-import Button from '../Button';
+import WideButton from '../WideButton';
 
 jest.mock('react-router-dom', () => ({
   Link: 'Link'
@@ -9,13 +9,13 @@ jest.mock('react-router-dom', () => ({
 
 describe('COMPONENT - BragButton', () => {
   it('renders Link if to prop is provided', () => {
-    const component = create(<Button variant="frontend" to="/frontend" />);
+    const component = create(<WideButton variant="frontend" to="/frontend" />);
 
     expect(component.toJSON()).toMatchSnapshot();
   });
 
-  it('render button if onClick handler is provided', () => {
-    const component = create(<Button variant="frontend" onClick={jest.fn()} />);
+  it('render button if onClick is provided', () => {
+    const component = create(<WideButton variant="frontend" onClick={jest.fn()} />);
 
     expect(component.toJSON()).toMatchSnapshot();
   });
