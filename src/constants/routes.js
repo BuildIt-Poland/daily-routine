72e5af27e1580a69a0d8f19e09ee7adf28371305
blurbyte <mateusz.lachacz@wipro.com>
2019-06-14 15:45:55 +0200

    Add routes brag and confess suffixes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,3 +2,7 @@ export const ROOT_PATH = '/';
 export const FRONT_END_ROLE_PATH = '/frontend';
 export const BACK_END_ROLE_PATH = '/backend';
 export const DEV_OPS_ROLE_PATH = '/devops';
+
+// Suffixes
+export const BRAG_PATH = '/brag';
+export const CONFESS_PATH = '/confess';
