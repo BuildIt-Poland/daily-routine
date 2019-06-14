2b2e93a599bd4be986c472e49e3d86ba67db4b2a
blurbyte <mateusz.lachacz@wipro.com>
2019-06-14 15:47:39 +0200

    Give birth to Robot character

---
+++ src/components/Characters/Robot/ConfusionMarks.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import ConfusionMarksBase from '../ConfusionMarks';
+
+const ConfusionMarks = styled(ConfusionMarksBase)`
+  top: -3.6rem;
+  left: 6rem;
+`;
+
+export default ConfusionMarks;
