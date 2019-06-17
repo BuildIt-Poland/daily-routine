a436f6bd1f4584edf138a260b198e22311a3ab53
blurbyte <mateusz.lachacz@wipro.com>
2019-06-17 12:22:30 +0200

    Create generic Hitbox component

---
+++ src/components/Hitbox/Hitbox.js
@@ -0,0 +1,19 @@
+// Button wrapper with all styles removed
+// Useful for accessible action icons / target areas
+
+import styled from 'styled-components';
+
+const Hitbox = styled.button`
+  border: 0;
+  margin: 0;
+  padding: 0;
+  background: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 4rem;
+  min-height: 4rem;
+  cursor: pointer;
+`;
+
+export default Hitbox;
