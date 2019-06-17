cce7918ef66f541d59dcebef997f0610843ad1a0
blurbyte <mateusz.lachacz@wipro.com>
2019-06-17 16:31:43 +0200

    Add quote to DevOps bubble

--- src/components/DevOpsRolePage/DevOpsRolePage.js
+++ src/components/DevOpsRolePage/DevOpsRolePage.js
@@ -8,10 +8,13 @@ import Default from './Default';
 import Brag from './Brag';
 import Confess from './Confess';
 
+// TODO Replace with auto-generated message @blurbyte
+const QUOTE = 'Yesterday I was implementing PUT method for missing RESTful API endpoint.';
+
 function DevOpsRolePage() {
   return (
     <Section>
-      <QuoteBubble />
+      <QuoteBubble quote={QUOTE} />
       <Route exact path={DEV_OPS_ROLE_PATH} component={Default} />
       <Route exact path={DEV_OPS_ROLE_PATH + BRAG_PATH} component={Brag} />
       <Route exact path={DEV_OPS_ROLE_PATH + CONFESS_PATH} component={Confess} />
