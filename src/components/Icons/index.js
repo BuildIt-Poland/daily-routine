fb8a54a2a5a3c3da79353cb98a5fed69d7951ba2
blurbyte <mateusz.lachacz@wipro.com>
2019-06-19 12:11:42 +0200

    Add Cross icon

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -3,13 +3,14 @@ export { default as Logo } from './Logo';
 
 // General
 export { default as Gear } from './Gear';
-export { default as LongArrow } from './LongArrow';
+export { default as Cross } from './Cross';
 export { default as CopyCards } from './CopyCards';
 export { default as Heart } from './Heart';
 export { default as GithubLogo } from './GithubLogo';
 export { default as BuilditLogo } from './BuilditLogo';
 
 // Buttons icons
+export { default as LongArrow } from './LongArrow';
 export { default as Lightbulb } from './Lightbulb';
 export { default as SaltGrinder } from './SaltGrinder';
 
