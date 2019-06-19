c6073fe803add232415561bcb8753e478eb778bb
Michal Pieniazek <michal.pieniazek@wipro.com>
2019-06-19 15:40:43 +0200

    [DR-45] Code-review comments: Mock components for cleaner snapshot

--- src/components/FrontEndRolePage/__tests__/FrontEndRolePage.test.js
+++ src/components/FrontEndRolePage/__tests__/FrontEndRolePage.test.js
@@ -8,6 +8,9 @@ import { FRONT_END_ROLE } from '../../../constants/roles';
 jest.mock('../../Characters', () => ({
   Fox: 'Fox'
 }));
+jest.mock('../../ActionButtons', () => 'ActionButtons');
+jest.mock('../../RoleBar', () => 'RoleBar');
+jest.mock('../../QuoteBubble', () => 'QuoteBubble');
 
 describe('COMPONENT - FrontEndRolePage', () => {
   it('renders correctly', () => {
