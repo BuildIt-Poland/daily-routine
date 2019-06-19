4d13ba86a7e4c268687de5fa1589117b9d4133c7
Mateusz Łachacz <info@blurbyte.com>
2019-06-19 12:18:37 +0200

    [DR-75] Close icon (#34)
    * Add Cross icon
    
    * Add test and snapshot

---
+++ src/components/Icons/Cross.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Cross() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path
+        fill="#222"
+        opacity="0.2"
+        d="M24 2.823L21.177 0 12 9.177 2.823 0 0 2.823 9.177 12 0 21.177 2.823 24 12 14.823 21.177 24 24 21.177 14.823 12z"
+      />
+    </svg>
+  );
+}
+
+export default Cross;
