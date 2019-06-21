b08a082e503d1f55fb00d7fa370b25c2298fb53a
blurbyte <mateusz.lachacz@wipro.com>
2019-06-21 11:50:31 +0200

    Add Wrapper for each character

---
+++ src/components/Characters/SugarCat/Wrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: 0;
+  margin-top: 4rem;
+  display: flex;
+  justify-content: center;
+`;
+
+export default Wrapper;
