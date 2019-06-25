8a69066cd8571318a64e245d6c7f2baa01d4e8ef
Maciej Hein <maciej.hein@wipro.com>
2019-06-25 20:22:43 +0200

    fix

--- src/components/SettingsButton/FemaleRadioLabel.js
+++ src/components/SettingsButton/FemaleRadioLabel.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 import { borderRadius } from '../../styles/designTokens';
 import RadioLabel from './RadioLabel';
 
-const FemaleLabel = styled(RadioLabel)`
+const FemaleRadioLabel = styled(RadioLabel)`
   border-radius: 0 ${borderRadius} ${borderRadius} 0;
 `;
 
-export default FemaleLabel;
+export default FemaleRadioLabel;
