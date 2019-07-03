fbb09939453fb46d152b64b15b8e04098528870e
blurbyte <mateusz.lachacz@wipro.com>
2019-07-03 11:33:18 +0200

    Responsive headlines

--- src/components/RoleBar/Header.js
+++ src/components/RoleBar/Header.js
@@ -1,14 +1,20 @@
 import styled from 'styled-components';
 
+import media from '../../styles/media';
 import { spacingMedium } from '../../styles/designTokens';
 
 const Header = styled.header`
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr auto;
   align-items: center;
-  justify-content: space-between;
-  padding: 0 1.4rem 0 ${spacingMedium};
+  /* compensate for medium spacing and gear icon width with left padding */
+  padding: 0 1.4rem 0 5.4rem;
   height: 5rem;
   background-color: rgba(255, 255, 255, 0.5);
+
+  ${media.phone`
+    padding-left: ${spacingMedium};
+  `};
 `;
 
 export default Header;
