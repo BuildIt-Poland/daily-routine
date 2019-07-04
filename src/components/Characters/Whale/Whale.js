5a65ebd156e6df0cf58f8be0ff059555eae1cee6
Marcin Goławski <marcin.golawski@wipro.com>
2019-07-04 15:34:35 +0200

    Fix tests and snapshots

--- src/components/Characters/Whale/Whale.js
+++ src/components/Characters/Whale/Whale.js
@@ -1,34 +1,27 @@
 import React, { useContext } from 'react';
-import styled, { keyframes } from 'styled-components';
 
 import { DEFAULT } from '../../../constants/roleActions';
 import { FEMALE } from '../../../constants/genders';
 import { pose } from '../../../types';
 import { GenderContext } from '../../../context/GenderContext';
-import Wrapper from './Wrapper';
+import FloatingAnimationWrapper from './FloatingAnimationWrapper';
 import WhaleArtwork from './WhaleArtwork';
+import AnimatedWhaleShadow from './AnimatedWhaleShadow';
 import ConfusionMarks from './ConfusionMarks';
 import GenderFlower from './GenderFlower';
-
-const floatingAnimation = keyframes`
-	0% { transform: translatey(0px); }
-	50% { transform: translatey(-40px); }
-	100% { transform: translatey(0px); }
-`;
-
-const AnimatedWrapper = styled(Wrapper)`
-  animation: ${floatingAnimation} 6s ease-in-out infinite;
-`;
 
 function Whale({ pose = DEFAULT }) {
   const { gender } = useContext(GenderContext);
 
   return (
-    <AnimatedWrapper>
-      <WhaleArtwork pose={pose} />
-      {pose === DEFAULT && <ConfusionMarks />}
-      {gender === FEMALE && <GenderFlower />}
-    </AnimatedWrapper>
+    <>
+      <AnimatedWhaleShadow />
+      <FloatingAnimationWrapper>
+        <WhaleArtwork pose={pose} />
+        {pose === DEFAULT && <ConfusionMarks />}
+        {gender === FEMALE && <GenderFlower />}
+      </FloatingAnimationWrapper>
+    </>
   );
 }
 
