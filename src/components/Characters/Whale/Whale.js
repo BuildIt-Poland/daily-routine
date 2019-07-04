416b28b666522764db919b2d1fdbd76373399dd4
Marcin Goławski <marcin.golawski@wipro.com>
2019-07-04 14:52:14 +0200

    Whale floating animation

--- src/components/Characters/Whale/Whale.js
+++ src/components/Characters/Whale/Whale.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import styled, { keyframes } from 'styled-components';
 
 import { DEFAULT } from '../../../constants/roleActions';
 import { FEMALE } from '../../../constants/genders';
@@ -9,15 +10,25 @@ import WhaleArtwork from './WhaleArtwork';
 import ConfusionMarks from './ConfusionMarks';
 import GenderFlower from './GenderFlower';
 
+const floatingAnimation = keyframes`
+	0% { transform: translatey(0px); }
+	50% { transform: translatey(-40px); }
+	100% { transform: translatey(0px); }
+`;
+
+const AnimatedWrapper = styled(Wrapper)`
+  animation: ${floatingAnimation} 6s ease-in-out infinite;
+`;
+
 function Whale({ pose = DEFAULT }) {
   const { gender } = useContext(GenderContext);
 
   return (
-    <Wrapper>
+    <AnimatedWrapper>
       <WhaleArtwork pose={pose} />
       {pose === DEFAULT && <ConfusionMarks />}
       {gender === FEMALE && <GenderFlower />}
-    </Wrapper>
+    </AnimatedWrapper>
   );
 }
 
