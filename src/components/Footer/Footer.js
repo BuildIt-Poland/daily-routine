c51a75cd7e310553865eabf8f58bcac5ab03e496
blurbyte <mateusz.lachacz@wipro.com>
2019-07-04 11:57:26 +0200

    Update Footer structure

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Heart, BuilditLogo, GithubLogo } from '../Icons';
+import Content from './Content';
 import Wrapper from './Wrapper';
 import Link from './Link';
 import DesktopOnly from './DesktopOnly';
@@ -8,16 +9,18 @@ import DesktopOnly from './DesktopOnly';
 function Footer() {
   return (
     <Wrapper>
-      <Link href="https://buildit.wiprodigital.com/" data-testid="buildit-home-page-link">
-        Crafted with <Heart /> by <BuilditLogo />
-      </Link>
-      <Link href="https://github.com/BuildIt-Poland/daily-routine" data-testid="github-link">
-        <GithubLogo />
-        <DesktopOnly>Contribute</DesktopOnly>
-      </Link>
-      <Link href="https://buildit.wiprodigital.com/careers/" data-testid="buildit-career-link">
-        We are hiring!
-      </Link>
+      <Content narrow>
+        <Link href="https://buildit.wiprodigital.com/" data-testid="buildit-home-page-link">
+          Crafted with <Heart /> by <BuilditLogo />
+        </Link>
+        <Link href="https://github.com/BuildIt-Poland/daily-routine" data-testid="github-link">
+          <GithubLogo />
+          <DesktopOnly>Contribute</DesktopOnly>
+        </Link>
+        <Link href="https://buildit.wiprodigital.com/careers/" data-testid="buildit-career-link">
+          We are hiring!
+        </Link>
+      </Content>
     </Wrapper>
   );
 }
--- src/components/Footer/Wrapper.js
+++ src/components/Footer/Wrapper.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
-import { spacingMedium } from '../../styles/designTokens';
+import media from '../../styles/media';
+import { colorWhite } from '../../styles/designTokens';
 
 const Wrapper = styled.footer`
-  height: 6rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 ${spacingMedium};
+  background: ${colorWhite};
+
+  ${media.phone`
+    background: inherit;
+  `};
 `;
 
 export default Wrapper;
