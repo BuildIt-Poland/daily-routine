d438ba46bd9efbe0d13c48b11d2cd0d6ed7afc4a
blurbyte <mateusz.lachacz@wipro.com>
2019-07-05 14:14:58 +0200

    Stick buttons to the bottom on the mobile

--- src/components/LandingPage/Page.js
+++ src/components/LandingPage/Page.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
+import media from '../../styles/media';
 import PageBase from '../Page';
 
 const Page = styled(PageBase)`
   grid-template-rows: minmax(46rem, max-content) 1fr;
+
+  ${media.phone`
+    grid-template-rows: 1fr auto;
+  `};
 `;
 
 export default Page;
