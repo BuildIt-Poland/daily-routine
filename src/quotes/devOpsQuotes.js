95d841b7efe6b8d5fdcb16de8facbf11caee1b40
blurbyte <mateusz.lachacz@wipro.com>
2019-07-09 16:11:44 +0200

    Add longer quotes

--- src/quotes/devOpsQuotes.js
+++ src/quotes/devOpsQuotes.js
@@ -1,5 +1,6 @@
 const QUOTES = {
-  '1': 'Yesterday I was implementing PUT method for missing RESTful API endpoint.'
+  '1':
+    "Yesterday I was implementing PUT method for missing RESTful API endpoint with complex database transactions. Also added experimental fundamental interface. I think we are missing good 'ol lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
 };
 
 export default QUOTES;
