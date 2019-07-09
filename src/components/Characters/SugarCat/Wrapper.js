a1153183a3991d94aac2f8c21d62d993f4e240d4
blurbyte <mateusz.lachacz@wipro.com>
2019-07-09 15:37:26 +0200

    Update SugarCat position

--- src/components/Characters/SugarCat/Wrapper.js
+++ src/components/Characters/SugarCat/Wrapper.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 
+import media from '../../../styles/media';
 import { spacingMedium } from '../../../styles/designTokens';
 
 const Wrapper = styled.div`
   position: relative;
   margin: 4rem auto 0 auto;
   width: 28rem;
-  padding-bottom: ${spacingMedium};
+  padding: ${spacingMedium} 0;
+
+  ${media.phone`
+    padding-top: 0;
+  `};
 `;
 
 export default Wrapper;
