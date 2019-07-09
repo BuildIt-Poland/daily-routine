1fe62214bc6972575ccd9b192ef6d58fec184caf
Marcin Goławski <marcin.golawski@wipro.com>
2019-07-09 14:35:28 +0200

    Whale tear animation

---
+++ src/components/Characters/Whale/Tear.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import TearBase from '../Tear';
+
+const ConfusionMarks = styled(TearBase)`
+  top: 21.3rem;
+  left: 7.4rem;
+`;
+
+export default ConfusionMarks;
