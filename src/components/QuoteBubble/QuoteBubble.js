4df2125bcf0b0356bb5a108e79a9036e88f85cff
blurbyte <mateusz.lachacz@wipro.com>
2019-07-09 15:10:57 +0200

    Responsive QuoteBubble component

--- src/components/QuoteBubble/QuoteBubble.js
+++ src/components/QuoteBubble/QuoteBubble.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { SPEECH } from '../../constants/speechBubbleVariant';
 import { speechBubbleVariant } from '../../types';
-import { SpeechBubble } from '../Icons';
+import { SPEECH } from '../../constants/speechBubbleVariant';
 import CopyButton from './CopyButton';
 import Wrapper from './Wrapper';
 import Quote from './Quote';
+import BubbleTail from './BubbleTail';
 
 const DEFAULT_QUOTE = "Daily in 5 minutes and I'm still not sure what to say...";
 
@@ -22,16 +22,16 @@ function QuoteBubble({ quote = DEFAULT_QUOTE, variant = SPEECH, noCopyToClipboar
   return (
     <Wrapper>
       <Quote>{quote}</Quote>
-      <SpeechBubble variant={variant} />
       {!noCopyToClipboard && <CopyButton onCopy={onCopy} />}
+      <BubbleTail variant={variant} />
     </Wrapper>
   );
 }
 
 QuoteBubble.propTypes = {
   quote: PropTypes.string,
-  variant: speechBubbleVariant,
-  noCopyToClipboard: PropTypes.bool
+  noCopyToClipboard: PropTypes.bool,
+  variant: speechBubbleVariant
 };
 
 export default QuoteBubble;
--- src/components/QuoteBubble/Wrapper.js
+++ src/components/QuoteBubble/Wrapper.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components';
-import { spacingMedium } from '../../styles/designTokens';
+
+import media from '../../styles/media';
+import SpeechBubbleArtwork from './SpeechBubbleArtwork';
 
 const Wrapper = styled.section`
-  position: absolute;
-  top: 0;
-  right: ${spacingMedium};
+  position: relative;
+  margin-bottom: 2rem;
+  margin-left: auto;
+  min-height: 9.6rem;
+  min-width: 21rem;
+  max-width: 36rem;
+  background: url(${SpeechBubbleArtwork}) no-repeat;
+  background-size: 100% 100%;
+
+  ${media.phone`
+    max-width: 100%;
+  `}
 `;
 
 export default Wrapper;
