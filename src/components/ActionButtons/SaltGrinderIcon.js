71a2f5cb6844160b36b1cabaf89e996477037699
Marcin Goławski <marcin.golawski@wipro.com>
2019-07-09 11:34:51 +0200

    [DR-41] List of messages for each character (#64)
    * Adds source file for role quotes
    
    * Changes to PR template
    
    * Change in pull request template
    
    * Fix ActionButton animation
    
    * Update snapshots
    
    * ESLint rule for underscore variables and arguments
    
    * pr template change

--- src/components/ActionButtons/SaltGrinderIcon.js
+++ src/components/ActionButtons/SaltGrinderIcon.js
@@ -8,12 +8,12 @@ import { SaltGrinder } from '../Icons';
 const Wrapper = styled.span`
   width: 8.8rem;
   position: absolute;
-  top: -0.9rem;
+  bottom: -0.9rem;
   right: -0.6rem;
 
   ${media.phone`
     width: 6.6rem;
-    top: -0.9rem;
+    bottom: -0.9rem;
     right: 0.4rem;
   `};
 `;
--- src/components/BackEndRolePage/BackEndBrag.js
+++ src/components/BackEndRolePage/BackEndBrag.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import sample from 'lodash.sample';
 
 import { BRAG } from '../../constants/roleActions';
 import { Robot } from '../Characters';
 import QuoteBubble from '../QuoteBubble';
 
+import QUOTES from '../../quotes/backEndQuotes';
+
 // TODO Replace with auto-generated message
-const QUOTE = 'Yesterday I was implementing PUT method for missing RESTful API endpoint.';
+const QUOTE = sample(QUOTES);
 
 function BackEndBrag() {
   return (
