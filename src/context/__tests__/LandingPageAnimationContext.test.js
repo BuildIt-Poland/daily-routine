176d3846317cee39198e4238e83c10589398960f
Michal Pieniazek <michal.pieniazek@wipro.com>
2019-07-11 06:39:51 +0200

    Update test snapshot

--- src/context/__tests__/LandingPageAnimationContext.test.js
+++ src/context/__tests__/LandingPageAnimationContext.test.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import { create } from 'react-test-renderer';
+import { render } from '@testing-library/react';
 
-import { LandingPageAnimationProvider } from '../LandingPageAnimationContext';
+import { LandingPageAnimationProvider, LandingPageAnimationContext } from '../LandingPageAnimationContext';
+import { ROOT_PATH } from '../../constants/routes';
 
 describe('COMPONENT - LandingPageAnimationProvider', () => {
-  it('renders correctly', () => {
-    const component = create(<LandingPageAnimationProvider></LandingPageAnimationProvider>);
+  it('should not render child component when "isAnimating" is "false"', () => {
+    const { container } = render(
+      <LandingPageAnimationProvider>
+        <LandingPageAnimationContext.Consumer>
+          {({ isAnimating }) => {
+            return isAnimating && <p>MockComponent</p>;
+          }}
+        </LandingPageAnimationContext.Consumer>
+      </LandingPageAnimationProvider>
+    );
 
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(container.querySelectorAll('p')[0]).not.toBeDefined();
+  });
+
+  it('should render child component when animateAndRedirect is called', () => {
+    const { container } = render(
+      <LandingPageAnimationProvider>
+        <LandingPageAnimationContext.Consumer>
+          {({ isAnimating, animateAndRedirect }) => {
+            animateAndRedirect(ROOT_PATH);
+            return isAnimating && <p>MockComponent</p>;
+          }}
+        </LandingPageAnimationContext.Consumer>
+      </LandingPageAnimationProvider>
+    );
+
+    expect(container.querySelectorAll('p')[0]).toHaveTextContent('MockComponent');
+  });
+
+  it('should not render child component when stopAnimation is called', () => {
+    const { container } = render(
+      <LandingPageAnimationProvider>
+        <LandingPageAnimationContext.Consumer>
+          {({ isAnimating, animateAndRedirect, stopAnimation }) => {
+            animateAndRedirect(ROOT_PATH);
+            stopAnimation();
+            return isAnimating && <p>MockComponent</p>;
+          }}
+        </LandingPageAnimationContext.Consumer>
+      </LandingPageAnimationProvider>
+    );
+
+    expect(container.querySelectorAll('p')[0]).not.toBeDefined();
   });
 });
