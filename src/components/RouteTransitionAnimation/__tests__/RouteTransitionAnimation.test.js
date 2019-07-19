7c91bfdabe3e45defa68fe3663ed515dad721b7d
blurbyte <mateusz.lachacz@wipro.com>
2019-07-19 14:57:55 +0200

    Update test and snapshot

--- src/components/RouteTransitionAnimation/__tests__/RouteTransitionAnimation.test.js
+++ src/components/RouteTransitionAnimation/__tests__/RouteTransitionAnimation.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
+import { ThemeProvider } from 'styled-components';
 
 import { RouteTransitionAnimationContext } from '../../../context/RouteTransitionAnimationContext';
 import RouteTransitionAnimation from '../RouteTransitionAnimation';
@@ -9,11 +10,17 @@ describe('COMPONENT - RouteTransitionAnimation', () => {
     const animateAndRedirect = () => {};
     const stopAnimation = () => {};
     const isAnimating = true;
+    const theme = {
+      primaryColor: '#fff',
+      secondaryColor: '#222'
+    };
 
     const component = create(
-      <RouteTransitionAnimationContext.Provider value={{ animateAndRedirect, isAnimating, stopAnimation }}>
-        <RouteTransitionAnimation />
-      </RouteTransitionAnimationContext.Provider>
+      <ThemeProvider theme={theme}>
+        <RouteTransitionAnimationContext.Provider value={{ animateAndRedirect, isAnimating, stopAnimation }}>
+          <RouteTransitionAnimation />
+        </RouteTransitionAnimationContext.Provider>
+      </ThemeProvider>
     );
 
     expect(component.toJSON()).toMatchSnapshot();
