692327a5361a1649aeb041bbb398aa3406039f79
Marcin Goławski <marcin.golawski@wipro.com>
2019-07-23 15:28:04 +0200

    Remove unnecessary bubble prop type

--- src/types/index.js
+++ src/types/index.js
@@ -1,6 +1,6 @@
 // Centralised type definitions for common cases
 
-import { bool, oneOf, shape, string, func } from 'prop-types';
+import { oneOf, shape, string, func } from 'prop-types';
 
 import { FRONT_END_ROLE, BACK_END_ROLE, DEV_OPS_ROLE } from '../constants/roles';
 import { DEFAULT, BRAG, CONFESS } from '../constants/roleActions';
@@ -29,9 +29,4 @@ export const history = shape({
 });
 
 export const speechBubbleVariant = oneOf([SPEECH, THOUGHT]);
-export const bubble = shape({
-  quote: string.isRequired,
-  quoteID: string.isRequired,
-  isInDefaultPose: bool.isRequired
-});
 export const socialMediaVariant = oneOf([FACEBOOK, TWITTER]);
