d68ddf1ca621126a84219911bb54c48770d6234f
Mateusz Łachacz <info@blurbyte.com>
2019-07-25 13:28:24 +0200

    [FIX] Quote bubble e2e (#89)
    * Update lockfile
    
    * Attempt to fix e2e

--- cypress/integration/QuoteBubble.test.js
+++ cypress/integration/QuoteBubble.test.js
@@ -16,14 +16,18 @@ describe('QuoteBubble', () => {
   describe('when Facebook share link is clicked', () => {
     it('should open suitable Facebook share window', () => {
       cy.getByTestId('facebook-share-button').click();
-      cy.get('@windowOpen').should('be.calledWithMatch', 'http://www.facebook.com/');
+      cy.get('@windowOpen').then(() => {
+        cy.get('@windowOpen').should('be.calledWithMatch', 'http://www.facebook.com/');
+      });
     });
   })
 
   describe('when Twitter share link is clicked', () => {
     it('should open suitable Twitter share window', () => {
       cy.getByTestId('twitter-share-button').click();
-      cy.get('@windowOpen').should('be.calledWithMatch', 'http://www.twitter.com/');
+      cy.get('@windowOpen').then(() => {
+        cy.get('@windowOpen').should('be.calledWithMatch', 'http://www.twitter.com/');
+      });
     });
   })
 
