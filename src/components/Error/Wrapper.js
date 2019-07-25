76ee25005b98f5af7cfd835ea893c456d82cde16
Michal Pieniazek <michal.pieniazek@wipro.com>
2019-07-25 15:18:41 +0200

    Add rounded corners

--- src/components/Error/Wrapper.js
+++ src/components/Error/Wrapper.js
@@ -7,19 +7,20 @@ import {
   fontWeightBold,
   spacingSmall,
   borderWidthThick,
-  colorLightRed
+  colorWhite
 } from '../../styles/designTokens';
 
 const Wrapper = styled.div`
   width: 100%;
   border: ${borderWidthThick} solid ${colorRed};
   color: ${colorBlack};
-  background-color: ${colorLightRed};
+  background-color: ${colorWhite};
   font-size: ${fontMedium};
   font-weight: ${fontWeightBold};
   padding: ${spacingSmall};
   display: flex;
   justify-content: center;
+  border-radius: 8px;
 `;
 
 export default Wrapper;
