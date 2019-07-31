5513b3942d3eaf7f6500cdf25c71a5b4fb1e4604
maciejhein <45594475+maciejhein@users.noreply.github.com>
2019-07-31 11:19:59 +0200

    [DR-124] Z-index ordering  (#104)
    * add z-index for elements with position absolute
    
    * update snapshots

--- src/components/GenderSettings/Input.js
+++ src/components/GenderSettings/Input.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
+import { zIndexDefault } from '../../styles/designTokens';
+
 const Input = styled.input`
   visibility: hidden;
   position: absolute;
+  z-index: ${zIndexDefault};
 `;
 
 export default Input;
--- src/components/RedirectButton/ArrowIcon.js
+++ src/components/RedirectButton/ArrowIcon.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 import media from '../../styles/media';
+import { zIndexDefault } from '../../styles/designTokens';
 import { LongArrow } from '../Icons';
 
 const Wrapper = styled.span`
   position: absolute;
   right: 1.4rem;
   bottom: 2.4rem;
+  z-index: ${zIndexDefault};
 
   ${media.phone`
     bottom:  1.4rem;
