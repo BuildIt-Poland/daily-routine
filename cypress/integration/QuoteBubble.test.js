7c94e3593cb16fad902c3ea0fc7a70fb6936900c
Michal Pieniazek <michal.pieniazek@wipro.com>
2019-08-05 11:55:10 +0200

    Run deploy only on master branch

--- cypress/integration/QuoteBubble.test.js
+++ cypress/integration/QuoteBubble.test.js
@@ -8,40 +8,34 @@ describe('QuoteBubble', () => {
   beforeEach(function() {
     cy.visit(`/${FRONT_END_ROLE}/${BRAG}`, {
       onBeforeLoad (win) {
-        cy.stub(win, 'open').as('windowOpen')
+        cy.stub(win, 'open').as('windowOpen');
       }
     });
     cy.getByTestId('frontend-brag-button').click();
   });
 
-  // Works locally
-  // Fails on CI due to:
-  // CypressError: cy.get() could not find a registered alias for: '@windowOpen'. You have not aliased anything yet.
   describe('when Facebook share link is clicked', () => {
     it('should open suitable Facebook share window', () => {
 
       cy.getByTestId('facebook-share-button').click();
       cy.get('@windowOpen').should('be.calledWithMatch', 'http://www.facebook.com/');
     });
-  })
+  });
 
-  // Works locally
-  // Fails on CI due to:
-  // CypressError: cy.get() could not find a registered alias for: '@windowOpen'. You have not aliased anything yet.
   describe('when Twitter share link is clicked', () => {
     it('should open suitable Twitter share window', () => {
       cy.getByTestId('twitter-share-button').click();
       cy.get('@windowOpen').should('be.calledWithMatch', 'http://www.twitter.com/');
     });
-  })
+  });
 
-  // Fails locally:
+  // FIXME: Skipped - fails locally:
   // Stuck on confirmation modal triggered by clicking on copy button
   xdescribe('when Copy-to-clipboard button is clicked ', () => {
     it('should show notification', () => {
       cy.getByTestId('copy-to-clipboard-button').click();
       cy.getByTestId('copy-notification').should('exist');
     });
-  })
+  });
 
 });
