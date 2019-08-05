013514d353a5cc440a62a1240bf00360f0ea3669
Marcin Goławski <marcin.golawski@wipro.com>
2019-08-05 14:13:59 +0200

    Update tests

--- src/hooks/__tests__/useLockBodyScroll.test.js
+++ src/hooks/__tests__/useLockBodyScroll.test.js
@@ -6,7 +6,7 @@ describe('HOOK - useLockBodyScroll', () => {
   it("sets body overflow to 'scroll' when falsy flag is passed", () => {
     renderHook(() => useLockBodyScroll(false));
 
-    expect(document.body.style.overflowY).toEqual('scroll');
+    expect(document.body.style.overflowY).toEqual('auto');
   });
 
   it("sets body overflow to 'hidden' when truthy flag is passed", () => {
