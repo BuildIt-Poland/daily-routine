a7fe9355254e923db907db34077173a20c994650
blurbyte <mateusz.lachacz@wipro.com>
2019-08-09 10:16:17 +0200

    Update RoleButtons

--- src/components/RedirectButton/RedirectButton.js
+++ src/components/RedirectButton/RedirectButton.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
+import { RouteTransitionAnimationContext } from '../../context/RouteTransitionAnimationContext';
 import Button from '../Button';
 import ArrowIcon from './ArrowIcon';
 
-function RedirectButton({ children, ...props }) {
+function RedirectButton({ children, to, ...props }) {
+  const { animateAndRedirect } = useContext(RouteTransitionAnimationContext);
+
+  function handleClick(e) {
+    e.preventDefault();
+    animateAndRedirect(to);
+  }
+
   return (
-    <Button {...props}>
+    <Button to={to} onClick={handleClick} {...props}>
       {children}
       <ArrowIcon />
     </Button>
@@ -14,7 +22,8 @@ function RedirectButton({ children, ...props }) {
 }
 
 RedirectButton.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  to: PropTypes.string.isRequired
 };
 
 export default RedirectButton;
--- src/components/RoleButtons/DevOpsButton.js
+++ src/components/RoleButtons/DevOpsButton.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import styled from 'styled-components';
 
 import { colorBlue, colorDarkBlue } from '../../styles/designTokens';
+import { DEV_OPS_ROLE, DEV_OPS_ROLE_LABEL } from '../../constants/roles';
 import RedirectButton from '../RedirectButton';
 
-const DevOpsButton = styled(RedirectButton)`
+const StyledButton = styled(RedirectButton)`
   background-color: ${colorBlue};
   border-bottom-color: ${colorDarkBlue};
 `;
+
+function DevOpsButton() {
+  return (
+    <StyledButton to={`/${DEV_OPS_ROLE}`} data-testid={`${DEV_OPS_ROLE}-button`}>
+      {DEV_OPS_ROLE_LABEL}
+    </StyledButton>
+  );
+}
 
 export default DevOpsButton;
